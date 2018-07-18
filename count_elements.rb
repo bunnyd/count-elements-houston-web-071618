0692e93f9dc411d8008de8facb731b06c9635445
github username <github email address>
2018-07-18 08:51:54 -0500

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -12,3 +12,14 @@ def count_elements(array)
   end
   new_hash
 end
+
+# def count_elements(array)
+#   count = Hash.new(0)
+#   array.each {|item| count[item] += 1 }
+#   count
+#
+#   # Fancy
+#   # array.each_with_object(Hash.new(0)) do |item, count|
+#   #   count[item] += 1
+#   # end
+# end
